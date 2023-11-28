6bda8ba297bd41b9c9fe75c9e56ad630688a29d3
bswitzer <brenna.switzer@gmail.com>
2023-11-27 21:06:32 -0800

    day nine part one

--- 2016/day09/test_day09.py
+++ 2016/day09/test_day09.py
@@ -20,3 +20,8 @@ class Test(TestCase):
 
     def test_p1_6(self):
         self.assertEqual(18, day09.part_one("X(8x2)(3x3)ABCY"))
+
+    def test_p1_actual(self):
+        filename = open("input.txt")
+        self.assertEqual(98135, day09.part_one(filename.read()))
+        filename.close()
