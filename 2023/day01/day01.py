13f06b0d3e971b1f11af69496851f6c3062351a6
bswitzer <brenna.switzer@gmail.com>
2023-11-30 22:08:02 -0800

    day 1 part 2 2023

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -18,14 +18,39 @@ def part_one(inputs):
 
 
 def part_two(inputs):
-    return 0
+    words = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+    total = 0
+    for text in inputs:
+        d = regex.findall(r'(\d|one|two|three|four|five|six|seven|eight|nine)', text, overlapped=True)
+        if len(d) == 0:
+            continue
+        if len(d) == 1:
+            first = d[0] if d[0].isnumeric() else words[d[0]]
+            num = first + first
+            total += int(num)
+        else:
+            first = d[0] if d[0].isnumeric() else words[d[0]]
+            second = d[-1] if d[-1].isnumeric() else words[d[-1]]
+            num = first + second
+            total += int(num)
+    return total
 
 
 def main():
     filename = open("input.txt")
     data = filename.read().splitlines()
     print(part_one(data))
-    # print(part_two())
+    print(part_two(data))
 
 
 if __name__ == '__main__':
--- 2023/day01/test_day01.py
+++ 2023/day01/test_day01.py
@@ -11,3 +11,18 @@ class Test(TestCase):
             "treb7uchet",
         ]
         self.assertEqual(142, day01.part_one(inputs))
+
+    def test_p1_2(self):
+        inputs = [
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"]
+        self.assertEqual(281, day01.part_two(inputs))
+
+    def test_p1_3(self):
+        inputs = ["pcg91vqrfpxxzzzoneightzt"]
+        self.assertEqual(98, day01.part_two(inputs))
